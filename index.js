e5f1054c848e9c9d82788110367073e14c9df887
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-02-03 00:00:54 -0500

    commit tree 7d204e7b24f05fb3979c22d676cf958afec00fef

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
+const { config } = require("chai");
+
 // Add your code here
+function submitData(userNameInput, emailInput){
+  let formData = {
+    name: userNameInput,
+    email: emailInput
+  };
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+
+    body: JSON.stringify(formData)
+  }
+
+  return fetch('http://localhost:3000/users', configObj).then(resp => resp.json()).then(json => {
+    let element = document.createElement('p');
+    element.innerHTML = json.id;
+    document.querySelector("body").appendChild(element);
+  }).catch(function(error) {
+    let element = document.createElement('p');
+    element.innerHTML = error.message;
+    document.querySelector("body").appendChild(element);
+  });
+};
+
